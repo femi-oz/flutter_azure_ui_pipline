1f415cd37697db93ebc13103085a38bba620f00e
femi-oz <femiosibodu@gmail.com>
2022-07-12 11:33:19 +0100

    Build fix

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ui_flutter
+# quick_flutter
 
 A new Flutter project.
 
--- android/app/src/main/java/com/example/ui_flutter/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.ui_flutter;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
